28bd6802841bba390c8a7e697ebd64b078f686a2
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-12 16:09:54 +0100

    close connection as IDE advised

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,6 @@ public class Main {
         studentGPA = newGPA.nextDouble();
         System.out.println(studentFirstName + " " +
                 studentLastName + " now has a GPA of " + studentGPA);
+        newGPA.close();
     }
 }
