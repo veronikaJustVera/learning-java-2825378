4e8a36c87779bca808b1de8bf4ab79d61d0023d6
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-12 13:39:55 +0100

    First compiling

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("First small program from Veronika ;)");
+		System.out.println("First small program from Veronika ;) This is changed");
 	}
 }
