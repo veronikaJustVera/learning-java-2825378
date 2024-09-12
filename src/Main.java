7ffdbe4d5c0055d31a7b48a177c7fb1ed96d8e65
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-12 15:44:45 +0100

    charAt function, ternary

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,18 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 20;
         double studentGPA = 85.5;
-        // char studentFirtInitial = 'V';
-        // char studentLastInitial = 'D';
-        boolean hasPerfectAttendance = false;
+        boolean hasPerfectAttendance = true;
         String studentFirstName = "Veronika";
         String studentLastName = "Demianenko";
         String studentName = studentFirstName + " " + studentLastName;
+        char studentFirtInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
         
-        String outputText = "Student " + studentName + " (" + studentAge + " y.o.) has GPA - " +  studentGPA;
-        if (hasPerfectAttendance) {
-            outputText += " and perfect attendance";
-        } else {
-            outputText += " but not perfect attendance";
-        }
+        String outputText = 
+            "Student " + studentFirtInitial + ". " + studentLastInitial + ". - "  + 
+            studentName + " (" + studentAge + " y.o.) has GPA - " +  studentGPA;
+
+        outputText += hasPerfectAttendance ? " and perfect attendance" : " but not perfect attendance";
         System.out.println(outputText);
     }
 }
