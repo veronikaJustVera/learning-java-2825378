bd7792d3ca65ebc92066085373205f9a2faa256a
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-12 15:37:53 +0100

    lesson 2.3

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 20;
         double studentGPA = 85.5;
-        char studentFirtInitial = 'V';
-        char studentLastInitial = 'D';
+        // char studentFirtInitial = 'V';
+        // char studentLastInitial = 'D';
         boolean hasPerfectAttendance = false;
+        String studentFirstName = "Veronika";
+        String studentLastName = "Demianenko";
+        String studentName = studentFirstName + " " + studentLastName;
         
-        String outputText = "Student with initials " + studentFirtInitial + ". " + studentLastInitial + 
-        ". (age " + studentAge + ") has GPA - " +  studentGPA;
+        String outputText = "Student " + studentName + " (" + studentAge + " y.o.) has GPA - " +  studentGPA;
         if (hasPerfectAttendance) {
             outputText += " and perfect attendance";
         } else {
