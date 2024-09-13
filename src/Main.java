5e3e618b28cf21413c96561cbe3ccbc4ac6a9895
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-13 20:37:28 +0100

    if condition, while loop, custom function

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int inputtedNum = getNumber();
+        while (inputtedNum < 1 || inputtedNum > 10) {
+            inputtedNum = getNumber();
+        }
+
+        if (inputtedNum > 5) {
+            System.out.println("Less or equals 5");
+        } else {
+            System.out.println("More than 5");
+        }
+    }
+
+    public static int getNumber() {
         System.out.println("Pick a number between 1 and 10");
         Scanner scanner = new Scanner(System.in);
 
         int inputtedNum = scanner.nextInt();
+        return inputtedNum;
     }
 }
