e1ece25543d6773e5febc15f2ecb038f8b62d4e6
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-13 20:45:30 +0100

    while loop

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? (Y/n)");
+            String userInput = input.next();
+            isOnRepeat = !userInput.equals("Y");
+        }
+        System.out.println("Playing next song");
     }
 }
