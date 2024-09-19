241d1414e4ad8d8bdfe30ced535d7a1c0e028e04
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 22:32:56 +0100

    calling a method

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(15, 8, 15, 12, 8);
         Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
+
+        double triangleAArea = triangleA.findArea(),
+               triangleBArea = triangleB.findArea();
     }
 
 }
