f1fb5763db3921c277096428550152d4d461177d
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 22:25:56 +0100

    creating instances

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Triangle triangleA = new Triangle(15, 8, 15, 12, 8);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
     }
 
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Triangle {
     double base;
     double height;
