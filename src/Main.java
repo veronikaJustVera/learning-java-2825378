9a6240e5c07a94736499b91b1fc1eaba47bccb9b
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 20:03:37 +0100

    built-in funcs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+package src;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        double result = Math.pow(2, 5);
+        System.out.println(result);
     }
 
 }
