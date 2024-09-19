1037a3566f8aeddf1da72c983b56a6893e88bd3c
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 16:12:50 +0100

    Syntax fixed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.Scanner;
 
 public class Main {
@@ -8,14 +10,14 @@ public class Main {
         String choiceTwo = "jupiter";
         String choiceThree = "saturn";
 
-        String correctAnswe = choiceThree;
+        String correctAnswer = choiceThree;
 
         // Write a print statement asking the question
         System.out.println(question);
 
         // Write a print statement giving the answer choices
         System.out.println("Choose one of the following: " +
-                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".")
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
         // Have the user input an answer
         Scanner scanner = new Scanner(System.in);
