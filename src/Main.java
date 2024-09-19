c6e682a3cacc0a50a99f8dbe46f782402fb71584
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 19:59:59 +0100

    call function

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
+package src;
+
 public class Main {
 
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double tipRate,
                                                double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
         System.out.println("Your total meal price is " + result);
+        return result;
     }
 
     public static void main(String[] args) {
-        
+        int numberOfPeople = 5;
+        double mealFinalPrice = calculateTotalMealPrice(100, .02, .08);
+        System.out.println("Price per 1 person for company of " + numberOfPeople + " is " + mealFinalPrice / numberOfPeople);
     }
 
 }
