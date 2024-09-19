d72bd8f4415fdffd67b8456bf09ff476365ba928
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 19:50:20 +0100

    function params

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+package src;
+
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double totalMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * totalMealPrice;
+        double tax = taxRate * totalMealPrice;
+        double result = totalMealPrice + tip + tax;
+        System.out.println("Total meal price: " + result + "$");
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(10, 0.1, 0.2);
     }
 
 }
