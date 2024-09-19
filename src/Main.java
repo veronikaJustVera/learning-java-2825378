a91d61e19b304d8a3b9cdfc5609c48d376af7f2f
Veronika Demianenko <veronika.demianenko@team.blue>
2024-09-19 19:33:37 +0100

    functions usage

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+package src;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void announceDeveloperTeaTime() {
+        System.out.println("announceDeveloperTeaTime executing...");
+        System.out.println("Type a random input to break.");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("announceDeveloperTeaTime execution stopped");
+    }
 
+    public static void main(String[] args) {
+        announceDeveloperTeaTime();
     }
 
 }
